bdd20604ef10860fc26aa477ddbb6f8591ff10c5
KaMu1 <st2_r@qq.com>
2021-06-20 07:13:10 +0800

    [web] logout

--- web/src/containers/context.tsx
+++ web/src/containers/context.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {TAppContext} from '../common/type/context/app-context';
+import {TUser} from '../common/type/user';
 
 type TContextWrapper = {
   children: JSX.Element | undefined
@@ -12,12 +13,22 @@ const contextValue = {
     id: '',
     anonymous_id: '',
   },
+  setUser: undefined,
 };
 
 export const AppContext = React.createContext<TAppContext>(contextValue);
 
 function ContextWrapper(props: TContextWrapper): JSX.Element {
-  return <AppContext.Provider value={contextValue}>
+  const [user, setUser] = useState<TUser & {isLogin: boolean}>(contextValue.user);
+
+  useEffect(() => {
+    const user = window.sessionStorage.getItem('user');
+    if (user !== null) {
+      setUser(JSON.parse(user));
+    }
+  }, []);
+
+  return <AppContext.Provider value={{user, setUser}}>
     {props.children}
   </AppContext.Provider>;
 }
