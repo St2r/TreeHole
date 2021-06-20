bb0fb11c8f4af17a4c9c414d4edab9da15368687
Sky <sky_py1044@hotmail.com>
2021-06-20 09:31:52 +0800

    [web] comment 样式

--- web/src/component/article/comment/index.tsx
+++ web/src/component/article/comment/index.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Chip } from '@material-ui/core';
 import React, {useState} from 'react';
 import {TComment} from '../../../common/type/comment';
 import ShowAll from '../show-all';
@@ -10,7 +11,21 @@ type TArticleCommentProps = {
 
 const commentItem = () => {
   // 一条评论，过长时隐藏
-  return <div>一条评论</div>;
+  return <div className="article-one-comment">
+    <div className="one-comment-left">
+      <Avatar
+        className="one-comment-avatar"
+        alt="Unknown"
+        src="https://avatars.githubusercontent.com/u/37372979?s=64&v=4"/>
+    </div>
+    <div className="one-comment-right">
+      <Chip className='one-comment-username' label={'username'} variant='outlined'/>
+      <Chip className='one-comment-publish-date' label={'2021-01-01'} variant='outlined' size='small'/>
+      <div className="one-comment-content">
+        一条评论一条评论一条评论一条评论一条评论一条评论一条评论一条评论一条评论一条评论一条评论一条评论一条评论
+      </div>
+    </div>
+  </div>;
 };
 
 function ArticleComment(props: TArticleCommentProps): JSX.Element {
